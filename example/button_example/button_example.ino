fc5de8de0932bbaaee7f43d1712e8e1f11afd6d3
kakmyc-github <39177565+kakmyc-github@users.noreply.github.com>
2019-11-01 16:58:34 +0400

    Update button_example.ino

--- example/button_example/button_example.ino
+++ example/button_example/button_example.ino
@@ -1,7 +1,7 @@
 #include <kakmyc_btn.h>
 
-btn Btn1(3,0,3);//1st button (pin 3, signal log0, max multiclick value 3)
-btn Btn2(4,1,5);//2nd button(pin 4, signal log1, max multiclick value 5)
+kakmyc_btn Btn1(3,INPUT,3);//1st button (pin 3, signal log0, max multiclick value 3)
+kakmyc_btn Btn2(4,INPUT_PULLUP,5);//2nd button(pin 4, signal log1, max multiclick value 5)
 /* LONGPRESS time>1.5sec
 click interval 0.3 sec
 LONGPRESS value=255 */
@@ -10,8 +10,8 @@ void setup(){
    
 }
 void loop(){
-    int BTN1=Btn1.key();
-    int BTN2=Btn2.key();
+    int BTN1=Btn1.read();
+    int BTN2=Btn2.read();
    if(BTN1) {Serial.print("btn1=");Serial.println(BTN1);}
        if(BTN2) {Serial.print("btn2=");Serial.println(BTN2);}
     
