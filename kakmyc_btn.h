b09b44d7b8133383f8ad5451f2399cfc48cc4535
kakmyc-github <39177565+kakmyc-github@users.noreply.github.com>
2019-11-01 11:50:06 +0400

    Add files via upload

---
+++ kakmyc_btn.h
@@ -0,0 +1,27 @@
+/* myBtn_constructor.h by kakmyc 2019©
+Library for processing signal from button 
+Types of signal: once/double/multiple/long */
+
+
+#ifndef kakmyc_btn_h
+#define kakmyc_btn_h
+#include <Arduino.h>
+
+class kakmyc_btn//создадим класс кнопок
+{public://общие функции и переменные
+    //конструктор класса
+    kakmyc_btn(byte pin,byte state,byte val);   
+    byte read();//функция обработчика нажатий
+private://личные переменные и функции класса
+    //локальные переменные обьекта номер пина/тип сигнала/количество кликов
+ byte _pin,_state,_val;
+ uint32_t start_press;//переменная времени нажатия кнопки
+ uint32_t pressTime;//переменная длительности нажатия кнопки
+ uint32_t double_press;//переменная времени между нажатиями
+ boolean pressFlag;//переменная флага нажатия
+ byte press_one;//переменная количества нажатий
+//переменные состояния/временного состояния/нажатия кнопки
+ byte num,_num,button; 
+};
+
+#endif
